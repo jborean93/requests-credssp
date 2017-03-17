909708d2263ebce6289b68718258ef44d766590a
Jordan Borean <jborean93@gmail.com>
2017-03-17 18:11:07 +1000

    changing version for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='requests-credssp',
-    version='0.0.1',
+    version='0.0.2',
     packages=[ 'requests_credssp' ],
     install_requires=[ 'ntlm-auth',
                        'ordereddict',
