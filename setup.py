f3494bd691733fdc1a59c1d5d24bac7607671450
Jordan Borean <jborean93@gmail.com>
2020-08-13 19:07:41 +1000

    Migrate CI to azure-pipelines

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
-# coding: utf-8
+# -*- coding: utf-8 -*-
+# Copyright: (c) 2020, Jordan Borean (@jborean93) <jborean93@gmail.com>
+# MIT License (see LICENSE or https://opensource.org/licenses/MIT)
+
+import os
 
 from setuptools import setup
 
-# PyPi supports only reStructuredText, so pandoc should be installed
-# before uploading package
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except ImportError:
-    long_description = ''
+
+def abs_path(rel_path):
+    return os.path.join(os.path.dirname(__file__), rel_path)
+
+
+with open(abs_path('README.md'), mode='rb') as fd:
+    long_description = fd.read().decode('utf-8')
+
 
 setup(
     name='requests-credssp',
@@ -31,22 +36,21 @@ setup(
             'gssapi>=1.5.0'
         ]
     },
-    python_requires='>=2.6,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     author='Jordan Borean',
     author_email='jborean93@gmail.com',
     url='https://github.com/jborean93/requests-credssp',
     description='HTTPS CredSSP authentication with the requests library.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords='authentication auth microsoft credssp winrm',
     license='MIT',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
